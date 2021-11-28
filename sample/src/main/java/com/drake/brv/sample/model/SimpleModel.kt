9056a32022375637b71239f8ca4f2b0ca3ab93cb
drake <liangjinggpx@gmail.com>
2021-11-28 15:47:54 +0800

    添加ViewBinding使用示例

--- sample/src/main/java/com/drake/brv/sample/model/SimpleModel.kt
+++ sample/src/main/java/com/drake/brv/sample/model/SimpleModel.kt
@@ -24,6 +24,10 @@ class SimpleModel(var name: String = "BRV") : ItemBind {
     override fun onBind(holder: BindingAdapter.BindingViewHolder) {
         // 不推荐这种方式, 因为Model只应该存在数据和逻辑, 如果包含UI绑定会导致视图耦合不例如项目迭代 (例如BRVAH)
         // val appName = holder.context.getString(R.string.app_name)
-        // holder.findView<TextView>(R.id.tv_simple).text = appName
+
+        // 使用不同的方法来获取视图控件
+        // holder.findView<TextView>(R.id.tv_simple).text = appName // 使用findById
+        // val viewBinding = ItemSimpleBinding.bind(holder.itemView) // 使用ViewBinding
+        // val dataBinding = holder.getBinding<ItemMultiTypeOneBinding>() // 使用DataBinding
     }
 }
---
+++ sample/src/main/java/com/drake/brv/sample/ui/fragment/ViewBindingFragment.kt
@@ -0,0 +1,40 @@
+package com.drake.brv.sample.ui.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.drake.brv.sample.R
+import com.drake.brv.sample.databinding.FragmentViewBindingBinding
+import com.drake.brv.sample.databinding.ItemDragBinding
+import com.drake.brv.sample.model.SimpleModel
+import com.drake.brv.utils.linear
+import com.drake.brv.utils.setup
+import com.drake.tooltip.toast
+
+/** 更推荐使用DataBinding做MVVM架构, ViewBinding只是取代findById的简单框架而已 */
+class ViewBindingFragment : Fragment(R.layout.fragment_view_binding) {
+
+    private val binding by lazy {
+        FragmentViewBindingBinding.bind(requireView())
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.rv.linear().setup {
+            addType<SimpleModel>(R.layout.item_simple)
+            onBind {
+                val binding = ItemDragBinding.bind(itemView) // 使用ViewBinding
+            }
+            R.id.tv_simple.onClick {
+                toast("点击文本")
+            }
+        }.models = getData()
+    }
+
+
+    private fun getData(): MutableList<Any> {
+        // 在Model中也可以绑定数据
+        return mutableListOf<Any>().apply {
+            for (i in 0..9) add(SimpleModel())
+        }
+    }
+}
