c2cf55af85e97e1247d4cc53aa36415ce7621ddd
彭少磊 <208964636@qq.com>
2023-04-21 20:16:47 +0800

    #356

--- sample/src/main/java/com/drake/brv/sample/ui/fragment/divider/GridDividerVerticalFragment.kt
+++ sample/src/main/java/com/drake/brv/sample/ui/fragment/divider/GridDividerVerticalFragment.kt
@@ -16,10 +16,12 @@
 
 package com.drake.brv.sample.ui.fragment.divider
 
+import com.drake.brv.annotaion.AnimationType
 import com.drake.brv.annotaion.DividerOrientation
 import com.drake.brv.sample.R
 import com.drake.brv.sample.databinding.FragmentGridDividerBinding
 import com.drake.brv.sample.model.DividerModel
+import com.drake.brv.utils.bindingAdapter
 import com.drake.brv.utils.divider
 import com.drake.brv.utils.grid
 import com.drake.brv.utils.setup
@@ -27,20 +29,33 @@ import com.drake.brv.utils.setup
 class GridDividerVerticalFragment :
     BaseDividerFragment<FragmentGridDividerBinding>(R.layout.fragment_grid_divider) {
 
+    private val dataList = mutableListOf<Any>()
+
     override fun initView() {
+        binding.btMainAdd.setOnClickListener {
+            dataList.add(DividerModel())
+            binding.rv.bindingAdapter.notifyItemInserted(dataList.size)
+            // 注释前的现象为问题1 注释后的现象为问题2
+            binding.rv.invalidateItemDecorations()
+        }
+        binding.btMainRemove.setOnClickListener {
+            dataList.removeAt(dataList.size - 1)
+            binding.rv.bindingAdapter.notifyItemRemoved(dataList.size)
+        }
         binding.rv.grid(3).divider {
             setDrawable(R.drawable.divider_horizontal)
             orientation = DividerOrientation.GRID
             includeVisible = true
         }.setup {
             addType<DividerModel>(R.layout.item_divider_vertical)
+            setAnimation(AnimationType.SLIDE_RIGHT)
+            animationRepeat = true
         }.models = getData()
     }
 
     fun getData(): MutableList<Any> {
-        return mutableListOf<Any>().apply {
-            for (i in 0..10) add(DividerModel())
-        }
+        for (i in 0..10) dataList.add(DividerModel())
+        return dataList
     }
 
     override fun initData() {
