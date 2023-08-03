9a7148f0d9b86777036fc8c321e8a0a3bf5437cd
drake <liangjinggpx@gmail.com>
2023-08-04 04:52:35 +0800

    sample: 优化ViewBindingFragment

--- sample/src/main/java/com/drake/brv/sample/model/SimpleBindingModel.kt
+++ sample/src/main/java/com/drake/brv/sample/model/SimpleBindingModel.kt
@@ -2,30 +2,12 @@ package com.drake.brv.sample.model
 
 import com.drake.brv.BindingAdapter
 import com.drake.brv.item.ItemBind
-import com.drake.brv.sample.R
-import com.drake.brv.sample.databinding.ItemSimpleBinding
 
-class SimpleBindingModel : ItemBind {
+class SimpleBindingModel() : ItemBind {
 
+    // 多类型列表请注意区分类型 https://liangjingkanji.github.io/BRV/multi-type.html#_5
     override fun onBind(holder: BindingAdapter.BindingViewHolder) {
-        // 不推荐这种方式, 因为Model只应该存在数据和逻辑, 如果包含UI绑定会导致视图耦合不例如项目迭代 (例如BRVAH)
-        // val appName = holder.context.getString(R.string.app_name)
-
-        // 使用不同的方法来获取视图控件
-        // holder.findView<TextView>(R.id.tv_simple).text = appName // 使用findById
-        // val dataBinding = holder.getBinding<ItemMultiTypeOneBinding>() // 使用DataBinding或ViewBinding
-
-        // 获取数据对象
-        // 如果存在多种数据类型, 请使用holder.getModelOrNull<Data>()或者if来判断itemViewType类型, 避免取值类型转换错误
-        // val data = holder.getModel<Data>()
-
-        when (holder.itemViewType) {
-            R.layout.item_simple_text -> {
-                val binding = holder.getBinding<ItemSimpleBinding>()
-                val data = holder.getModel<SimpleModel>()
-                binding.tv.text = data.name
-            }
-        }
-
+        // val binding = holder.getBinding<ItemViewBindingBinding>()
+        // binding.tvSimple.text = holder.layoutPosition.toString()
     }
 }
--- sample/src/main/java/com/drake/brv/sample/ui/fragment/ViewBindingFragment.kt
+++ sample/src/main/java/com/drake/brv/sample/ui/fragment/ViewBindingFragment.kt
@@ -3,11 +3,9 @@ package com.drake.brv.sample.ui.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.viewbinding.ViewBinding
 import com.drake.brv.sample.R
 import com.drake.brv.sample.databinding.FragmentViewBindingBinding
-import com.drake.brv.sample.databinding.ItemCommentBinding
-import com.drake.brv.sample.databinding.ItemSimpleTextBinding
+import com.drake.brv.sample.databinding.ItemViewBindingBinding
 import com.drake.brv.sample.model.SimpleBindingModel
 import com.drake.brv.utils.linear
 import com.drake.brv.utils.setup
@@ -22,22 +20,10 @@ class ViewBindingFragment : Fragment(R.layout.fragment_view_binding) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.rv.linear().setup {
-            addType<SimpleBindingModel>(R.layout.item_simple_text)
+            addType<SimpleBindingModel>(R.layout.item_view_binding)
             onBind {
-
-                // 单一类型不用判断
-                val binding = getBinding<ItemSimpleTextBinding>() // 使用ViewBinding/DataBinding都可以使用本方法
+                val binding = getBinding<ItemViewBindingBinding>()
                 binding.tvSimple.text = layoutPosition.toString()
-
-                // 如果是多类型可以通过判断ViewBinding类型分开处理
-                when (val viewBinding = getBinding<ViewBinding>()) {
-                    is ItemSimpleTextBinding -> {
-                        viewBinding.tvSimple.text = layoutPosition.toString()
-                    }
-                    is ItemCommentBinding -> {
-                        viewBinding.tvContent.text = layoutPosition.toString()
-                    }
-                }
             }
             R.id.tv_simple.onClick {
                 toast("点击文本")
