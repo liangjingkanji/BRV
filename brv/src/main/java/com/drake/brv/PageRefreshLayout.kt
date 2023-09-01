f772a3e2886f2d8b60faf8292f85d0d291642f46
drake <liangjinggpx@gmail.com>
2023-09-01 12:15:24 +0800

    pref: setNoMoreData循环调用

--- brv/src/main/java/com/drake/brv/PageRefreshLayout.kt
+++ brv/src/main/java/com/drake/brv/PageRefreshLayout.kt
@@ -468,9 +468,13 @@ open class PageRefreshLayout : SmartRefreshLayout, OnRefreshLoadMoreListener {
     // 解决在Activity的onAttachedToWindow未调用时SmartRefreshLayout下拉刷新立即完成导致的循环调用堆栈溢出
     private var currentNoMoreData: Boolean? = null
     override fun setNoMoreData(noMoreData: Boolean): RefreshLayout {
-        if (currentNoMoreData != noMoreData) {
-            currentNoMoreData = noMoreData
+        if (mAttachedToWindow) {
             super.setNoMoreData(noMoreData)
+        } else {
+            if (currentNoMoreData != noMoreData) {
+                currentNoMoreData = noMoreData
+                super.setNoMoreData(noMoreData)
+            }
         }
         return this
     }
